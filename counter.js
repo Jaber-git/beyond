252d359a28094689c5fcb72208a1a80cf0358e8a
Jaber-git <post2Jaber-git@users.noreply.github.com>
2019-03-08 21:20:40 +0600

    different syntax

--- counter.js
+++ counter.js
@@ -1,7 +1,26 @@
+
+//we can move onclick event on this main js instead on DOM
+//document.addEventListener("DomContentLoaded", function(){ // fot loading whole page
+ //   document.querySelector("button").onclick=count;
+
+//})
+//or
 let counter=0;
+document.addEventListener("DomContentLoaded", function() { 
+    document.querySelector("button").onclick=function(){
+                    counter++;
+                    document.querySelector("#counter").innerHTML=counter; //current value of counter=0 and it incrementing
 
-function count() {
-counter++;
-document.querySelector("#counter").innerHTML=counter; //current value of counter=0 and it incrementing
 
-}
+    };
+       })//it take some element and wait something to happen
+
+
+
+
+//or  it is simple
+//function count() {
+//counter++;
+//document.querySelector("#counter").innerHTML=counter; //current value of counter=0 and it incrementing
+
+//}
