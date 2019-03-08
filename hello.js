1372ccd04e0affe26f6f009630eb3bae8cab6d13
Jaber-git <post2Jaber-git@users.noreply.github.com>
2019-03-08 20:10:40 +0600

    this is function and tap manipulation

--- hello.js
+++ hello.js
@@ -1,2 +1,11 @@
-let x=50;
-alert(x);
+// variables
+//let x=50;
+//alert(x);
+/* Manipulating Dom*/ 
+// function
+ function hello (){
+  alert("hello");
+  //manipulate tag
+  document.querySelector("h1").innerHTML="Welcome changed to Hello"
+
+ }
