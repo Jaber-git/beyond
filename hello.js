97acefcc9034e1c46af4c9572bfc45b8c9dce6dc
Jaber-git <post2Jaber-git@users.noreply.github.com>
2019-03-08 20:24:56 +0600

    for loop

--- hello.js
+++ hello.js
@@ -3,9 +3,20 @@
 //alert(x);
 /* Manipulating Dom*/ 
 // function
- function hello (){
-  alert("hello");
+ //function hello (){
+ // alert("hello");
   //manipulate tag
-  document.querySelector("h1").innerHTML="Welcome changed to Hello"
+ // document.querySelector("h1").innerHTML="Welcome changed to hello";
 
- }
+// }
+ function hello (){
+    
+            //get all the heading and it's an array and store array into headings
+            const headings =document.querySelectorAll("h1");
+            // each one heading( 2 h1) loop through
+            for(const heading of headings){
+            heading.innerHTML="Hello";
+
+             }
+  
+   }
